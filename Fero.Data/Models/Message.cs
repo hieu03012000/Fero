9b3fe50ca308c461b477d1571ef0b468998ff35c
TPMLong <trieuminhlong2000@gmail.com>
2021-06-20 21:31:41 +0700

    "test nha"

--- Fero.Data/Models/Message.cs
+++ Fero.Data/Models/Message.cs
@@ -5,6 +5,7 @@ namespace Fero.Data.Models
 {
     public partial class Message
     {
+        //333333
         public int Id { get; set; }
         public string MsgContent { get; set; }
         public DateTime Date { get; set; }
